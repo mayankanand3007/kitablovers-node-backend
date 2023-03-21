d80a7cf4d7b5944a6ac102e8a4fb4a78e7f670c6
Mayank Anand <mayankan@gmail.com>
2023-03-21 18:03:21 +0530

    [Mayank] Add: Error Handling

---
+++ backend/middleware/error.js
@@ -0,0 +1,12 @@
+import ErrorHandler from "../utils/errorhandler.js";
+
+export default (err, req, res, next) => {
+    err.statusCode = err.statusCode || 500;
+    err.message = err.message || "Internal Server Error";
+
+    res.status(err.statusCode).json({
+        success:false,
+        error: err.stack,
+        message: err.message
+    });
+};
