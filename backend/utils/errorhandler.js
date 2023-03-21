91e4cc8c63902bb4619f921f8eece93c0c7f844a
Mayank Anand <mayankan@gmail.com>
2023-03-21 18:04:37 +0530

    [Mayank] Add: Utilities for Error Handler

---
+++ backend/utils/errorhandler.js
@@ -0,0 +1,8 @@
+export default class ErrorHandler extends Error{
+    constructor(message, statusCode){
+        super(message);
+        this.statusCode = statusCode;
+
+        ErrorHandler.captureStackTrace(this, this.constructor);
+    }
+}
